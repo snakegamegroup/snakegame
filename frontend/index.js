f3078a810f3c10634431fa207ce22328167c882a
snakegamegroup <81927173+snakegamegroup@users.noreply.github.com>
2021-04-08 18:37:10 -0400

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -1,94 +1,92 @@
-//Colours used in the game window
 const BG_COLOUR = '#000000';
-const SNAKE_COLOUR = '#357CF8'
-const SNAKE_COLOUR1 = '#AF10DA'
-const FOOD_COLOUR = '#871E10';
+const SNAKE_COLOUR = '#E6A218';
+const SNAKE_COLOUR1 = '#AF10DA';
+const FOOD_COLOUR = '#EE1818';
 
-//socket
-const socket = io(' https://sleepy-falls-52998.herokuapp.com/');
+const socket = io('https://sheltered-island-91254.herokuapp.com/');
 
-
-//When the socket is used handle all possible outcomes 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
 socket.on('gameOver', handleGameOver);
 socket.on('gameCode', handleGameCode);
 socket.on('unknownCode', handleUnknownCode);
-socket.on('tooManyPlayers', handleT);
-//Constants for screens and other elements 
+socket.on('tooManyPlayers', handleTooManyPlayers);
+
+const gameOverScreen = document.getElementById('gameOverScreen');
 const gameScreen = document.getElementById('gameScreen');
 const initialScreen = document.getElementById('initialScreen');
+const winnerScreen = document.getElementById('winnerScreen');
 const newGameBtn = document.getElementById('newGameButton');
 const joinGameBtn = document.getElementById('joinGameButton');
 const gameCodeInput = document.getElementById('gameCodeInput');
 const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+const loserHomeScreenBtn = document.getElementById('loserHomeScreenButton');
+const homeScreenBtn = document.getElementById('homeScreenButton');
 
-//game buttons 
 newGameBtn.addEventListener('click', newGame);
 joinGameBtn.addEventListener('click', joinGame);
+loserHomeScreenBtn.addEventListener('click', home);
+homeScreenBtn.addEventListener('click', home);
 
-
-//function to create a new game by calling init
 function newGame() {
   socket.emit('newGame');
   init();
 }
 
-//function to join a game using the code given
 function joinGame() {
   const code = gameCodeInput.value;
   socket.emit('joinGame', code);
   init();
 }
 
-//declare variables  used 
+function home(){
+  reset();
+}
+
+
 let canvas, ctx;
 let playerNumber;
 let gameActive = false;
 
-//init function on creation of new game
+window.addEventListener("keydown", function(e) {
+  // space and arrow keys
+  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+  e.preventDefault();
+}
+}, false);
 function init() {
-  //change displays
   initialScreen.style.display = "none";
   gameScreen.style.display = "block";
+  gameOverScreen.style.display = "none";
+  winnerScreen.style.display = "none";
 
-  //get the canvas and set it to a 2d 
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
 
-  //make the canvas 600 pixels high
   canvas.width = canvas.height = 600;
 
-  //set the canvas background to black
   ctx.fillStyle = BG_COLOUR;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-  //add and event listener for keydowns
+
   document.addEventListener('keydown', keydown);
   gameActive = true;
 }
 
-//fucntion to handle keydowns
 function keydown(e) {
-  socket.emit('keydown', e.keyCode);//send the key stroke to the server
+  socket.emit('keydown', e.keyCode);
 }
 
-//function to paint the game
 function paintGame(state) {
-  //fill the background
   ctx.fillStyle = BG_COLOUR;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  //constants used for food, grid, and size
   const food = state.food;
   const gridsize = state.gridsize;
   const size = canvas.width / gridsize;
 
-  //fill the food with the colour and fill the rectangle
   ctx.fillStyle = FOOD_COLOUR;
   ctx.fillRect(food.x * size, food.y * size, size, size);
 
-  //paint the players 
   paintPlayer(state.players[0], size, SNAKE_COLOUR);
   paintPlayer(state.players[1], size, SNAKE_COLOUR1);
 }
@@ -123,9 +121,9 @@ function handleGameOver(data) {
   gameActive = false;
 
   if (data.winner === playerNumber) {
-    alert('You Win');
+    winner();
   } else {
-    alert('You have Lost ');
+    gameOver();
   }
 }
 
@@ -135,12 +133,12 @@ function handleGameCode(gameCode) {
 
 function handleUnknownCode() {
   reset();
-  alert('Game Code Invalid')
+  alert('Unknown Game Code')
 }
 
 function handleTooManyPlayers() {
   reset();
-  alert('Game in Progress');
+  alert('This game is already in progress');
 }
 
 function reset() {
@@ -148,4 +146,16 @@ function reset() {
   gameCodeInput.value = '';
   initialScreen.style.display = "block";
   gameScreen.style.display = "none";
+  gameOverScreen.style.display = "none";
+  winnerScreen.style.display = "none";
+}
+
+function gameOver(){
+  gameScreen.style.display = "none";
+  gameOverScreen.style.display = "block";
+}
+
+function winner(){
+  gameScreen.style.display = "none";
+  winnerScreen.style.display = "block";
 }
