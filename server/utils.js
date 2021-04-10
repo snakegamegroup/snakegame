39d8d42edc71d1d65ef63d54b52ca705cbccbb5d
snakegamegroup <81927173+snakegamegroup@users.noreply.github.com>
2021-04-10 11:22:28 -0400

    Update utils.js

--- server/utils.js
+++ server/utils.js
@@ -1,21 +1,13 @@
 module.exports = {
-    makeid,
-  }
-  /* Function to randomize and create a game code */
-  
-  function makeid(length) {
-     //set the result string to empty
-     var result           = '';
-     //set of possible characters
-     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-     var charactersLength = characters.length;//get the characters length 
-     
-     //for the length given make a game id that long 
-     for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength)); //selects random characters to generate an id
-     }
-  
-     //return id
-     return result;
-  }
-  
+   makeid,
+ }
+ 
+ function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
