20957d04015ef4a5dab26aa98ebcdcfa9f97151f
snakegamegroup <81927173+snakegamegroup@users.noreply.github.com>
2021-04-10 02:12:39 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ const SNAKE_COLOUR = '#E6A218';
 const SNAKE_COLOUR1 = '#AF10DA';
 const FOOD_COLOUR = '#EE1818';
 
-const socket = io(' https://sleepy-falls-52998.herokuapp.com/');
+const socket = io('https://hidden-headland-31336.herokuapp.com/');
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
 socket.on('gameOver', handleGameOver);
